e1506a4d9dd2116d06a07b3a1cca791d68df6b8a
Herbert Ntim <herbertntim15@gmail.com>
2024-01-01 09:53:46 +0000

    clerk authentication in process

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -20,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,30 @@
+import { SignedOut } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link
+          href='/'
+          className="w-36"
+        >
+          <Image src="/assets/images/logo.svg" alt="evently logo" width={128} height={38} />
+        </Link>
+
+        <div className="w-32 flex items-center justify-end gap-3">
+          <SignedOut>
+            <Button asChild className=''>
+              <Link href={'/sign-in'}>
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
   )
 }
 
