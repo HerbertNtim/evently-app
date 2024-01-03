652b5d84c81723863435aefa7920c4750920d473
Herbert Ntim <herbertntim15@gmail.com>
2024-01-03 12:53:08 +0000

    working on creating events

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,15 @@
+const CreateEvent = () => {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-center bg-cover">
+        <h3 className="wrapper h3-bold text-center sm:text-left py-5 md:py-10">Create Your Events Here</h3>
+      </section>
+
+      <div className="wrapper my-8">
+
+      </div>
+    </>
+  )
+}
+
+export default CreateEvent
