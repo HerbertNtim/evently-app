e564ba5e319ed38fc96ed106a91514fc1d07c8c6
Herbert Ntim <herbertntim15@gmail.com>
2024-01-03 23:08:52 +0000

    working on forms validation

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,4 +1,11 @@
+import EventForm from "@/components/shared/EventForm"
+import { auth } from "@clerk/nextjs"
+
 const CreateEvent = () => {
+  const { sessionClaims } = auth()
+
+  const userId = sessionClaims?.userId as string
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-center bg-cover">
@@ -6,7 +13,7 @@ const CreateEvent = () => {
       </section>
 
       <div className="wrapper my-8">
-
+        <EventForm userId={userId} type='Create'/>
       </div>
     </>
   )
