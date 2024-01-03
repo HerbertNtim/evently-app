83e990c01384d24bfb5b2b8339dd1b3ac9bde1cc
Herbert Ntim <herbertntim15@gmail.com>
2024-01-03 23:29:17 +0000

    forms setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: 'Evently',
   description: 'Evently is an event platform',
-  icons: '/assets/images/logo.svg'
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
 }
 
 export default function RootLayout({
