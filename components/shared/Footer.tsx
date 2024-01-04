73ed68bad5c9e86403bcd39a3aabcc48703a7796
Herbert Ntim <herbertntim15@gmail.com>
2024-01-04 22:00:14 +0000

    working on react-datepicker

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
 
   return (
     <footer className="border-t">
-      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+      <div className="flex-center wrapper flex-between flex flex-col gap-3 text-center sm:flex-row">
         <Link href='/'>
           <Image 
             src="/assets/images/logo.svg"
